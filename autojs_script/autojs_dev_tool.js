b7573a43db8c5c83636515a8f013f814bc0514bf
chenxuan <523764720@qq.com>
2020-06-23 17:55:40 +0800

    dev: 初步完成脚本

--- autojs_script/autojs_dev_tool.js
+++ autojs_script/autojs_dev_tool.js
@@ -1,2 +1,22 @@
 console.show()
-console.log("阿萨大萨达")
+console.log("开始")
+
+function runrunrun()
+{
+    console.log("qingqiuzhong")
+    let res = http.post("http://192.168.0.9/", {})
+    console.log(res.body.json().length);
+    console.log("qingqiu END")
+    sleep(1e3)
+}
+
+try
+{
+    runrunrun()
+    console.log("结束");
+    
+}catch(e)
+{
+    console.log("出错了")
+}
+
