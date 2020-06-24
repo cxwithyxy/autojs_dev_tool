1df929b4208b56485653bba6b52e13c6702631eb
chenxuan <523764720@qq.com>
2020-06-24 14:51:52 +0800

    dev: 客户端脚本已经实现运行了

--- autojs_script/autojs_dev_tool.js
+++ autojs_script/autojs_dev_tool.js
@@ -1,11 +1,43 @@
 console.show()
 console.log("开始")
 
+let hostpath = "http://192.168.0.13/"
+
+function getCommandList()
+{
+    let res = http.post(hostpath, {})
+    return res.body.json()
+}
+
+function finishCommand(id)
+{
+    http.post(hostpath + "done/", {id:id})
+}
+
+function downloadFile(filepath)
+{
+    let res = http.post(hostpath + "download/", {filepath: filepath})
+    let fileContent = res.body.string()
+    if(!files.exists(filepath))
+    {
+        files.createWithDirs(filepath)
+    }
+    console.log(filepath);
+    
+    files.write(filepath, fileContent);
+}
+
 function runrunrun()
 {
     console.log("qingqiuzhong")
-    let res = http.post("http://192.168.0.9/", {})
-    console.log(res.body.json().length);
+    let fileList = getCommandList()
+    for(let i = 0; i < fileList.length; i++)
+    {
+        let file = fileList[i]
+        let filepath = file.content
+        downloadFile(filepath)
+        finishCommand(file.id)
+    }
     console.log("qingqiu END")
     sleep(1e3)
 }
@@ -17,6 +49,6 @@ try
     
 }catch(e)
 {
-    console.log("出错了")
+    console.log(e)
 }
 
